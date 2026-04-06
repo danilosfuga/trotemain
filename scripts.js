f1f7aba3d6005b532369b169f63ba6a5a002a5e3
Danilo Silva Fuga <danio.s.fuga.profissional@gmail.com>
2026-04-06 20:33:53 -0300

    v11

--- scripts.js
+++ scripts.js
@@ -5,19 +5,34 @@ function calcular(){
                 let qtdeMacarrao = Number(document.getElementById("qtdeMacarrao").value)
                 let pontos = (qtdeArroz * 10) + (qtdeFeijao * 8) + (qtdeOleo * 4) + (qtdeMacarrao * 2)
 
-                document.getElementById("resultado").innerText = "Pontuação Total: " + pontos + " pontos"
-
+                let cor = document.getElementById("cor").value
+                let metaprova1
+                if(cor === "amarela"){
+                    metaprova1 = 54
+                }
+                else if(cor === "cinza"){
+                    metaprova1 = 51
+                }
+                else if(cor === "laranja"){
+                    metaprova1 = 21
+                }
+                else if(cor === "marron"){
+                    metaprova1 = 88
+                }
+                else if(cor === "preta"){
+                    metaprova1 = 60
+                }
 
                 let kitAlimentacao = Number(document.getElementById("kitAlimentacao").value)
-                if(kitAlimentacao >= 60){
+                if(kitAlimentacao >= metaprova1){
                     pontos = pontos + 5000
-                    if(kitAlimentacao > 60){
-                        pontos = pontos + (kitAlimentacao -60) * 83.33
+                    if(kitAlimentacao > metaprova1){
+                        pontos = pontos + (kitAlimentacao - metaprova1) * (5000 / metaprova1)
+                    }
                 }
                 else{
-                    pontos = pontos + kitAlimentacao * 83.33
+                    pontos = pontos + kitAlimentacao * (5000 / metaprova1)
                 }
-
-
                 document.getElementById("resultado").innerText = "Pontuação Total: " + pontos + " pontos"
-            }
+               
+               }
